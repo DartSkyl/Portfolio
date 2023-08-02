d4df0cd5d5b4c5ab3a7751bc544e458ba03f0825
Evgeny Grabko <blackferd@gmail.com>
2023-08-02 17:26:31 +0300

    fix(api): deleted an extra variable

--- utils/site_api.py
+++ utils/site_api.py
@@ -3,12 +3,11 @@ from handlers.custom_handlers.output_of_the_result import start_output
 
 
 def get_exercise(url, headers, querystring):
-    response = requests.get(url=url, headers=headers, params={'muscle': 'neck'})
+    response = requests.get(url=url, headers=headers, params=querystring)
     return response.json()
 
 
 def main_api_func(url, headers, querystring, bot, from_user_id):
-    ready_string = ''
     ready_list = []
     for elem in get_exercise(url, headers, querystring):
         ready_string = elem['name'] + '\n' + 'Equipment: ' + elem['equipment'] + '\n' + elem['instructions']
