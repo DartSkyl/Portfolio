0ba374e2493dbbbd3a9fe6d4397def9dfa3a8e1b
Evgeny Grabko <blackferd@gmail.com>
2023-08-02 17:20:52 +0300

    fix(api): added passing variables through function arguments feat(database): added interaction with the database feat(diary): started implementig a training diary

--- handlers/custom_handlers/write.py
+++
@@ -1,8 +0,0 @@
-from telebot.types import Message
-
-from loader import bot
-
-
-@bot.message_handler(commands=["write"])
-def bot_start(message: Message):
-    bot.reply_to(message, f"Здесь {message.from_user.full_name} ведет запись в дневник!")
